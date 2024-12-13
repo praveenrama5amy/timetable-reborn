5551dd0f231470769c21f5aa7f5338056f837a3b
UCannotFindMe <praveenramasamy123@gmail.com>
2024-12-13 07:37:44 +0530

    new timetable

--- client/src/pages/Timetable.tsx
+++ client/src/pages/Timetable.tsx
@@ -1,39 +1,26 @@
-import { useEffect, useMemo, useRef, useState } from "react"
-import { ClassType, FacultyType, SubjectType, useAppDataContext } from "../context/AppDataContext"
-import useTimetable from "../hooks/useTimetable"
-import useAppData from "../hooks/useAppData"
+import { useEffect, useState } from "react"
+import useConflict from "../hooks/useConflict"
+import { ConflictInterface } from "../context/AppDataContext"
+import "../css/Timetable.css"
 
 const Timetable = () => {
-    const [department, setDepartment] = useAppDataContext().department
-    const [conflicts] = useAppDataContext().conflicts
-    const [profileSelected] = useAppDataContext().profile
-
-    const Timetable = useTimetable()
-    const { fetchDepatment } = useAppData()
-
-    const [typeSelected, setTypeSelected] = useState<"class" | "faculty">("class")
-    const [selected, setSelected] = useState<ClassType | FacultyType | undefined | null>(typeSelected == "class" ? department?.classes[0] : department?.faculties[0])
-    const [error, setError] = useState<null | string>()
-
-    const [checked, setChecked] = useState<Array<{ classId: ClassType['id'], day: number, hour: number }>>([])
-
+    const Conflict = useConflict()
+    const [conflicts, setConflicts] = useState<ConflictInterface[]>([])
     useEffect(() => {
-        if (error != null) {
-            setTimeout(() => {
-                setError(null)
-            }, 5000)
-        }
-    }, [error])
-
+        Conflict.getConflict().then(res => {
+            if (res.conflicts) {
+                setConflicts(res.conflicts)
+            }
+        })
+    }, [])
     if (conflicts.length > 0) return <div className="h-100">
         <p className="text-4xl text-center font-medium">Conflicts</p>
         <p className="mt-10 ml-5 text-lg font-medium text-red-500">There is some conflict in the given inputs. Correct the conflicts to proceed further</p>
         <p className="mt-10 text-right mr-5 text-lg font-medium text-red-500">Total Conflicts : {conflicts.length}</p>
         <div className="container">
             {conflicts.map((conflict, i) =>
-                <div key={i} className="text-base font-medium border-2 p-8">
+                <div key={i} className="text-base font-medium border-2 p-8 rounded-md">
                     {/* <p>{conflict.type}</p> */}
-
                     <p className="text-xl underline -ml-2">{conflict.error}</p>
                     <p>{conflict.maker.name} #{conflict.maker.id}</p>
                     <p className="text-red-600">{conflict.message}</p>
@@ -45,107 +32,31 @@ const Timetable = () => {
             )}
         </div>
     </div>
-    const handle = {
-        assign: async (classId: ClassType['id'], subjectId: SubjectType['id'], day: number, hour: number) => {
-            const res = await Timetable.assign(classId, subjectId, day, hour)
-            console.log(res);
-            if (res.status && res.status.success) {
-                fetchDepatment(profileSelected!)
-            }
-            if (res.error) {
-                setError(res.error.message)
-            }
-        },
-        unassign: async (classId: ClassType['id'], day: number, hour: number) => {
-            const res = await Timetable.unassign(classId, day, hour)
-            if (res.status && res.status.success) {
-                fetchDepatment(profileSelected!)
-            }
-        },
-        toggleCheck: (classId: ClassType['id'], day: number, hour: number) => {
-            if (checked.find(e => e.classId == classId && e.day == day && e.hour == hour)) {
-                setChecked(checked.filter(e => e.classId != classId || e.day != day || e.hour != hour))
-            } else {
-                setChecked([...checked, { classId, day, hour }])
-            }
-        },
-        unassignAll: () => {
-            checked.forEach(e => {
-                handle.unassign(e.classId, e.day, e.hour)
-            })
-            setChecked([])
-        }
-    }
     return (
         <div>
-            <p className="text-xl font-medium">Type : </p>
-            <div className="dropdown">
-                <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    {typeSelected}
-                </button>
-                <ul className="dropdown-menu">
-                    <li onClick={() => { setTypeSelected("class") }}><button className="dropdown-item" type="button">Class</button></li>
-                    {/* <li onClick={() => { setTypeSelected("faculty") }}><button className="dropdown-item" type="button">Faculty</button></li> */}
-                </ul>
+            <p className="text-3xl font-medium text-center">Timetable</p>
+            <div className="border-2 border-black  rounded-lg m-10 flex flex-col">
+                <div className="flex flex-row border-b-2 p-4 border-primary">
+                    <p className="font-bold text-lg text-center flex-1">Day/Hour</p>
+                    {/* <p className="font-bold text-lg text-center flex-1">Day/Hour</p> */}
+                </div>
+                <div className="flex flex-row border-b-2 p-4 border-primary">
+                    <p className="font-bold text-lg text-center flex-1">Day 1</p>
+                </div>
             </div>
-            <p className="text-xl font-medium capitalize">{typeSelected} : </p>
-            <div className="dropdown">
-                <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    {selected?.name || "Select"}
-                </button>
-                <ul className="dropdown-menu">
-                    {department?.classes.map(room =>
-                        <li key={room.id} onClick={() => { setSelected(room) }}><button className="dropdown-item" type="button">{room.name}</button></li>
-                    )}
-                </ul>
-            </div>
-            {checked.length > 0 && <div>
-                <button className="btn btn-dark m-5" onClick={() => {
-                    handle.unassignAll()
-                }}>Unassign All</button>
-            </div>}
-            <p className="text-red-600 font-medium text-xl">{error}</p>
-            {typeSelected && selected && <div>
-                {
-                    typeSelected == "class" ?
-                        <div>
-                            <p className="text-xl font-bold">{selected.name}</p>
-                            <table className="table table-bordered">
-                                <tbody>
-                                    {department?.classes.find(e => e.id == selected.id)?.timetable.map((day, i) => <tr key={i}>
-                                        {day.map((hour, j) =>
-                                            <td key={j} className="relative">
-                                                <input type="checkbox" className="absolute top-0 right-0" checked={checked.find(e => e.classId == selected.id && e.day == i && e.hour == j) ? true : false} onChange={e => { handle.toggleCheck(selected.id, i, j) }} />
-                                                <div className="dropdown">
-                                                    <button className={`btn  dropdown-toggle ${department.subjects.find(e => e.id == hour) == null ? 'btn-outline-secondary' : 'btn-secondary'}`} type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                                        {department.subjects.find(e => e.id == hour)?.name || "Select"}
-                                                    </button>
-                                                    <ul className="dropdown-menu">
-                                                        {department.classes.find(e => e.id == selected.id)?.subjects.map(sub =>
-                                                            <li className="p-2" onClick={() => {
-                                                                handle.assign(selected.id, sub.subject, i, j)
-                                                            }} key={sub.subject}><a className="dropdown-item" href="#">
-                                                                    {department.subjects.find(e => e.id == sub.subject)?.name}
-                                                                </a></li>
-                                                        )}
-                                                        <li className="p-2" onClick={() => {
-                                                            handle.unassign(selected.id, i, j)
-                                                        }}><a className="dropdown-item text-red-600" href="#">Delete</a></li>
-                                                    </ul>
-                                                </div>
-                                            </td>
-                                        )}
-                                    </tr>)}
-                                </tbody>
-                            </table>
-                        </div>
-                        :
-                        <div>
-
-                        </div>
-                }
-            </div>}
-        </div >
+            <table className="border-2 border-black">
+                <thead>
+                    <tr>
+                        <th>Day/Hour</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     )
 }
 
